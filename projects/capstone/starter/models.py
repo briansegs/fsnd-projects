2744d7ef9d192f3cc839a22722f64bedadaa88e4
brian <briansegs@gmail.com>
2021-02-06 13:01:17 -0500

    Add drop and create db

--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,1 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+'''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+'''
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
 
