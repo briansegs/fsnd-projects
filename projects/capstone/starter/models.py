b71f76ea97e168fbc0dd7e38b2cd10ccc9770ca8
brian <briansegs@gmail.com>
2021-02-06 14:59:18 -0500

    Add Actor and Edits

--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
+
 
 '''
 db_drop_and_create_all()
@@ -10,6 +12,7 @@ db_drop_and_create_all()
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
+
 
 class Movies(db.Model):
     __tablename__ = 'movies'
@@ -50,8 +53,57 @@ class Movies(db.Model):
         deletes a model from the database
         the model must exist in the database
         EXAMPLE:
-            movie = Movie(title=req_title, release_date=req_recipe)
+            movie = Movie.query.filter(Movie.id == id).one_or_none()
             movie.delete()
+    '''
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Actors(db.Model):
+    __tablename__ = 'actors'
+    # Autoincrementing, unique primary key
+    id = db.Column(db.Integer, primary_key=True)
+    # String Name
+    name = db.Column(db.String, nullable = False)
+    # Integer Age
+    age = db.Column(db.Integer, nullable = False)
+    # String Gender
+    gender = db.Column(db.String, nullable = False)
+
+    '''
+    insert()
+        inserts a new model into a database
+        the model must have a unique id or null id
+        the model must have a title (not null) and release_date (not null)
+        EXAMPLE:
+            actor = Actor(name=req_name, age=req_age, gender=reg_age)
+            actor.insert()
+    '''
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    update()
+        updates a model in the database
+        the model must exist in the database
+        EXAMPLE:
+            actor = Actor.query.filter(Actor.id == id).one_or_none()
+            actor.name = 'Samson'
+            actor.update()
+    '''
+    def update(self):
+        db.session.commit()
+
+    '''
+    delete()
+        deletes a model from the database
+        the model must exist in the database
+        EXAMPLE:
+            actor = Actor.query.filter(Actor.id == id).one_or_none()
+            actor.delete()
     '''
     def delete(self):
         db.session.delete(self)
