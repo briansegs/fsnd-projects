06c782e887a5e3741d4e5b661332a9125dc4215e
brian <briansegs@gmail.com>
2021-02-06 15:23:18 -0500

    Add setup_db

--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,7 +1,20 @@
+from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 
 
+database_name = "casting"
+database_path = "postgresql://{}:{}@{}/{}".format('brian', 'admin', 'localhost:5432', database_name)
 db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
 
 
 '''
