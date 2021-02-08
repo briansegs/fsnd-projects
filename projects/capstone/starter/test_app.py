9a3ecc9dec83e38aa189a743b585008d893f5e3f
brian <briansegs@gmail.com>
2021-02-08 18:09:37 -0500

    Add patch movie and actor

--- projects/capstone/starter/test_app.py
+++ projects/capstone/starter/test_app.py
@@ -250,6 +250,28 @@ class TestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertTrue(data['actor'])
 
+    def test_producer_patch_movie(self):
+        res = self.client().post('/movies', headers=self.producer_jwt, json=self.new_movie)
+        data = json.loads(res.data)
+
+        res = self.client().patch('/movies/1', headers=self.producer_jwt, json=self.new_title)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['movie'])
+
+    def test_producer_patch_actor(self):
+        res = self.client().post('/actors', headers=self.producer_jwt, json=self.new_actor)
+        data = json.loads(res.data)
+
+        res = self.client().patch('/actors/1', headers=self.producer_jwt, json=self.new_name)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['actor'])
+
     # def test_405_invalid_method(self):
     #     res = self.client().patch('/categories')
     #     data = json.loads(res.data)
