dd4c939acfeeff24b9758054ffe116457b51a22d
brian <briansegs@gmail.com>
2021-02-09 16:05:36 -0500

    Add

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -254,10 +254,10 @@ def create_app(test_congig=None):
 
 	return app
 
-APP = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run()
 
 
 
